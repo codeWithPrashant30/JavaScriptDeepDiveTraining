795119208452d96dd0b44a20efa03da70cd9e591
codeWithPrashant30 <palfronted300593@gmail.com>
2025-12-02 21:12:02 +0530

    Update React app with new components and styles

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { StudentList } from './component/StudentList';
 import styled from 'styled-components';
+import logo from '../src/logo.svg'
 
 function App() {
+
   const students = [
     { id: 1, name: "Prashant", age: 25 },
     { id: 2, name: "Aman", age: 23 },
@@ -30,12 +32,40 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
+const images = [
+  { src: "/assets/logo.png", alt: "logo1" },
+  { src: "/assets/logo.png", alt: "logo2" },
+  { src: "/assets/logo.png", alt: "logo3" },
+];
+
+
+const DisplayImages = ({ images }) => {
+  console.log(images)
+  return (
+    <div className="flex justify-center flex-wrap gap-4 border">
+      { images.map((img, index) => (
+          <img
+            key={index}
+            src={img.src}
+            style={{border:"1px solid #ccc"}}
+            alt={img.alt || "image"}
+            className="w-40 h-40 object-cover rounded-lg"
+          />
+        ))}
+    </div>
+  );
+};
+
   // let arr = [1,2,3]
   return (
     <div className="App">
-      <h1>
-        Hello world!
-      </h1>
+      <header className="flex justify-center">
+        <img src={logo} alt='logo'  className="w-20 h-20 "/>
+      </header>
+     <DisplayImages images={images}/>
+
+      <h1 class="text-5xl font-bold text-green-500">MERN STACK | 25 days</h1>
+
         <p>This is my first React Application</p>
         {/* <Welcome greetings="Hello Everyone, How are you ?" myYTchannelName="codeWithPrashant1"  arr = {arr}/> */}
         <Button $primary>Primary Button</Button>
--- React/my-app/src/component/StudentList.js
+++ React/my-app/src/component/StudentList.js
@@ -1,17 +1,21 @@
 import style from "./Student.module.css"
+// import s from '.././component/Student.module.css'
 
 export const StudentList = ({ students }) => {
   return (
-    <ul className={style.list} style={{listStyle:"none"}}>
+   <>
+   {/* <h2 className={s.h2}>Hello H2</h2> */}
+    <ul className="bg-gray-200 text-gray-800 p-4 rounded-lg">
       {students.map((student, index) => {
         return (
-          <li style={{"border":"1px solid black"}} key={index}>
-            id: {student.id} <br />
-            name: {student.name} <br />
-            age: {student.age}
+          <li className="bg-white shadow-md rounded-md p-4 mb-4" key={index}>
+            <p className="text-lg font-semibold">ID: {student.id}</p>
+            <p className="text-md">Name: {student.name}</p>
+            <p className="text-md">Age: {student.age}</p>
           </li>
         );
       })}
     </ul>
+    </>
   );
 };
